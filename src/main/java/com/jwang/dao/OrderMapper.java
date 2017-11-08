5029d59992d665c2e578260890fb7579386929bd
wjd-java <929155002@qq.com>
2017-11-09 00:12:57 +0800

    git init commit

---
+++ src/main/java/com/jwang/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.jwang.dao;
+
+import com.jwang.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/jwang/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.jwang.dao;
+
+import com.jwang.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
